7d22cbce820ba19c9b827a417f0449f2e66b2659
MarySheng <mdiosgalla@gmail.com>
2020-03-27 13:49:30 +0800

    building content with JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
+// Import the React and ReactDOM libraries
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+ 
+  
+// Create a react component
+
+const App = () => {
+    const buttontext = 'Click me'; // javascript variable
+    const labelText = 'Enter name:'; // javascript variable
+    const labelText2 = 'Email:';
+    const labelText3 = 'Password:';
+  return (
+  <div>
+  <label className= "label" htmlFor = "name">
+      {labelText} {/*invocating javascript*/}
+      </label> 
+   <input id ="name" type = "text"></input>
+    
+      <br></br>
+      <br></br>
+      <label className ="label" htmlFor = "email">
+        {labelText2}
+      </label>
+       <input id = "email" type = "text"></input>
+       <br></br>
+       <br></br>
+       <label classname = "label" htmlFor= "password">
+      {labelText3}
+    </label>
+  <input id = "password" type = "text"></input>
+  <br></br>
+   <br></br>
+  <button style= {{backgroundColor:"blue", color:"white"}}>
+    {buttontext} {/*invocating javascript*/}
+</button>
+</div> 
+  )
+}
+
+
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+     <App />,
+     document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
